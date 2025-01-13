420a8b762b8b30c1a81e9f9c745af2390d786e27
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-01-13 23:03:02 +0500

    Update

--- core/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import register, TranslationOptions
-from .models import NewsAndEvents, ActivityLog
-
-@register(NewsAndEvents)
-class NewsAndEventsTranslationOptions(TranslationOptions):
-    fields = ('title', 'summary',)
-    empty_values=None
-
