7eccfd9898d540fe070bbcba2ce05810dc716860
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-01-16 15:40:01 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -107,8 +107,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "zomin_lms_baza",
+        "USER": "zomin_lms_baza_user",
+        "PASSWORD": "lzfGWKWOiolyIxUZAfL3u895RnMjrtPm",
+        "HOST": "dpg-cu2lrspu0jms73apm6b0-a.oregon-postgres.render.com",
+        "PORT": "5432",
     }
 }
 
