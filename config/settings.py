d5078e6e32b9702a4fb6e9b295dfa5b7afbf562e
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-01-20 23:13:58 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -101,10 +101,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
