79791e3fd57329696b948fd70ed530655e304047
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-01-29 02:24:09 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,30 +1,27 @@
 
 import os
+# from decouple import config
 from django.utils.translation import gettext_lazy as _
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "sdvsdv5dvd1v5d4sv2sd4v2sdvsd4v2sdv424"
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# change the default user models to our custom model
 AUTH_USER_MODEL = "accounts.User"
 
+# Application definition
 
 DJANGO_APPS = [
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     "modeltranslation",  
-=======
-    "modeltranslation",  # Tarjimon
->>>>>>> Stashed changes
-=======
-    "modeltranslation",  # Tarjimon
->>>>>>> Stashed changes
-=======
-    "modeltranslation",  # Tarjimon
->>>>>>> Stashed changes
     "jet.dashboard",
     "jet",
     "django.contrib.admin",
